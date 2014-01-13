c6f09a8bfa4bb7c48e669cdb14606f6ca0c6deab
Phoenix Nemo <mrphoenixlzx@gmail.com>
2014-01-13 14:53:23 +0800

    simple index

--- static/js/functions.js
+++
@@ -1,5 +0,0 @@
-// Random background-image
-var images = [
-    '01.jpg'
-];
-$('div.content').css({'background-image': 'url(static/img/background/' + images[Math.floor(Math.random() * images.length)] + ')' });
